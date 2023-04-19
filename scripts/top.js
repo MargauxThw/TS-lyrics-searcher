4c96c049b1f4e80db5da7eea29a96cf936fef7f4
MargauxThw <36945000+MargauxThw@users.noreply.github.com>
2023-04-19 20:57:56 +1000

    Add scroll to top tracking

--- scripts/top.js
+++ scripts/top.js
@@ -4,7 +4,7 @@
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -18,4 +18,9 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  window.goatcounter.count({
+    path: 'Scroll to top',
+    title: 'action',
+    event: true,
+  })
 }
