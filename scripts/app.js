2afc57f6ac2e46b8084ddeda43583c0f2b3a844d
MargauxThw <36945000+MargauxThw@users.noreply.github.com>
2023-07-07 18:46:16 +1000

    Update for SNTV

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,10 @@
-fetch('https://raw.githubusercontent.com/MargauxThw/TS-lyrics/main/AllDataMay2723.json')
+fetch('https://raw.githubusercontent.com/MargauxThw/TS-lyrics/main/AllDataJuly723.json')
     .then(response => response.json())
     .then(data => runApp(data))
     .catch(err => console.log(err));
 
 
-const album_order = ["midnights", "evermore", "folklore", "Lover", "Reputation", "1989", "Red (Taylor's Version)", "Speak Now", "Fearless (Taylor's Version)", "Taylor Swift", "Unreleased", "Singing Credits Only", "Talkshow Parody", "Unspecified Album", "EP: Sounds Of The Season: The Taylor Swift Holiday Collection"]
+const album_order = ["midnights", "evermore", "folklore", "Lover", "Reputation", "1989", "Red (Taylor's Version)", "Speak Now (Taylor's Version)", "Fearless (Taylor's Version)", "Taylor Swift", "Unreleased", "Singing Credits Only", "Talkshow Parody", "Unspecified Album", "EP: Sounds Of The Season: The Taylor Swift Holiday Collection"]
 
 const num_albums = 10
 
